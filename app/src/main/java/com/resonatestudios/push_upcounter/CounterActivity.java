4c64e1245a57be71fe79757479a0068e4e202874
Muhammad Nabillah <abel.rischa@gmail.com>
2018-12-09 10:35:18 +0700

    Initial commit

---
+++ app/src/main/java/com/resonatestudios/push_upcounter/CounterActivity.java
@@ -0,0 +1,69 @@
+package com.resonatestudios.push_upcounter;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class CounterActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private Sensor sensorProximity;
+
+    TextView textViewProximityReadings;
+    TextView textViewCounter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_counter);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorProximity = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+        textViewProximityReadings = findViewById(R.id.text_view_proximity_readings);
+        textViewProximityReadings.setText("");
+        textViewCounter = findViewById(R.id.text_view_counter);
+        textViewCounter.setText("Jumlah push-up: 0");
+
+
+        if (sensorProximity != null) {
+            // ada sensor accelerometer
+            AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+            alertDialog.setMessage("Sukses, device punya sensor proximity");
+            alertDialog.show();
+        } else {
+            // tidak ada sensor accelerometer
+            AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+            alertDialog.setMessage("TIdak ada sensor proximity");
+            alertDialog.show();
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, sensorProximity, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        double distance = event.values[0];
+        textViewProximityReadings.setText(String.valueOf(distance));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
